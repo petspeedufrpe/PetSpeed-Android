80b46ce0d51a092ea7e05d588bb074935171becd
NEWUSER <caiofariaspe@gmail.com>
2019-05-10 21:04:07 -0300

    Crud Pessoa Endereco Usuario e Cliente

---
+++ app/src/main/java/br/ufrpe/bsi/mpoo/petSpeed/negocio/ClienteServices.java
@@ -0,0 +1,76 @@
+package br.ufrpe.bsi.mpoo.petSpeed.negocio;
+
+import java.util.List;
+
+import br.ufrpe.bsi.mpoo.petSpeed.dominio.Cliente.Animal;
+import br.ufrpe.bsi.mpoo.petSpeed.dominio.Cliente.Cliente;
+import br.ufrpe.bsi.mpoo.petSpeed.dominio.Endereco.Endereco;
+import br.ufrpe.bsi.mpoo.petSpeed.persistencia.AnimalDAO;
+import br.ufrpe.bsi.mpoo.petSpeed.persistencia.ClienteDAO;
+import br.ufrpe.bsi.mpoo.petSpeed.persistencia.EnderecoDAO;
+
+public class ClienteServices {
+
+	private ClienteDAO clienteDAO;
+
+	private EnderecoDAO enderecoDAO;
+
+	private AnimalDAO animalDAO;
+
+	public void cadastraCliente() {
+
+	}
+
+	public void deletaCliente() {
+
+	}
+
+	public Endereco getEnderecoById() {
+		return null;
+	}
+
+	public Animal getAnimalById() {
+		return null;
+	}
+
+	public Animal getAnimalByRaca() {
+		return null;
+	}
+
+	public List<Endereco> getAllEndereco() {
+		return null;
+	}
+
+	public List<Animal> getAllAnimal() {
+		return null;
+	}
+
+	public void removeEndereco() {
+
+	}
+
+	public void adicionaEndereco() {
+
+	}
+
+	public Cliente getClienteById() {
+		return null;
+	}
+
+	public Cliente getClienteByEmail() {
+		return null;
+	}
+
+	public void alteraEmail() {
+
+	}
+
+	public void alteraSenha() {
+
+	}
+
+	public void alteraAvaliacao() {
+
+	}
+
+}
---
+++ app/src/main/java/br/ufrpe/bsi/mpoo/petSpeed/negocio/EnderecoServices.java
@@ -0,0 +1,34 @@
+package br.ufrpe.bsi.mpoo.petSpeed.negocio;
+
+import br.ufrpe.bsi.mpoo.petSpeed.dominio.Endereco.Endereco;
+import br.ufrpe.bsi.mpoo.petSpeed.persistencia.EnderecoDAO;
+
+public class EnderecoServices {
+
+	private EnderecoDAO enderecoDAO;
+
+	public void cadastraEndereco() {
+
+	}
+
+	public void deletaEndereco() {
+
+	}
+
+	public void alteraCep() {
+
+	}
+
+	public void alteraNumero() {
+
+	}
+
+	public void alteraComplemento() {
+
+	}
+
+	public Endereco getEnderecoById() {
+		return null;
+	}
+
+}
