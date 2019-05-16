591d113dc69a7229917f6f7383a55f6ffe0943c2
Yasmmin Claudino <ymclaudino@gmail.com>
2019-05-15 21:08:11 -0300

    tela login

--- app/src/main/java/br/ufrpe/bsi/mpoo/petSpeed/infra/gui/LoginActivity.java
+++ app/src/main/java/br/ufrpe/bsi/mpoo/petSpeed/infra/gui/LoginActivity.java
@@ -2,14 +2,18 @@ package br.ufrpe.bsi.mpoo.petSpeed.infra.gui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import br.ufrpe.bsi.mpoo.petSpeed.R;
 
 public class LoginActivity extends AppCompatActivity {
+	private String[] tipoUsuario = new String[]{"Clínica", "Veterinário", "Cliente"};
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
+
 	}
 }
