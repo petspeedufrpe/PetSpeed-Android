3a33e033938a2004f54f8145de83a08604f69ef9
Yasmmin Claudino <ymclaudino@gmail.com>
2019-05-16 10:25:15 -0300

    redirecionando telas login

--- app/src/main/java/br/ufrpe/bsi/mpoo/petSpeed/infra/gui/MainActivity.java
+++ app/src/main/java/br/ufrpe/bsi/mpoo/petSpeed/infra/gui/MainActivity.java
@@ -1,8 +1,10 @@
 package br.ufrpe.bsi.mpoo.petSpeed.infra.gui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import br.ufrpe.bsi.mpoo.petSpeed.R;
@@ -19,97 +21,37 @@ import br.ufrpe.bsi.mpoo.petSpeed.usuario.persistencia.UsuarioDAO;
 
 public class MainActivity extends AppCompatActivity {
 	private DBHelper dbHelper = new DBHelper();
+    private Button butClinica;
+    private Button butVet;
+    private Button butCliente;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		/**
-		long codCurr;
-		long codPessoa;
-		Endereco endereco = new Endereco();
-		endereco.setCep("123456");
-		endereco.setNumero(121);
-		endereco.setComplemento("Rua 2");
-		EnderecoDAO enderecoDAO= new EnderecoDAO();
-		codCurr =enderecoDAO.cadastraEndereco(endereco);
-		endereco.setId(codCurr);
-		Pessoa pessoa = new Pessoa();
-		pessoa.setCpf("012213124");
-		pessoa.setNome("Caio castro");
-		pessoa.setEndereco(endereco);
-		PessoaDAO pessoaDAO = new PessoaDAO();
-		codPessoa =pessoaDAO.cadastraPessoa(pessoa);
-		pessoa.setId(codPessoa);
-		Usuario usuario = new Usuario();
-		usuario.setEmail("teste@gmail.com");
-		usuario.setSenha("12345");
-		UsuarioDAO usuarioDAO = new UsuarioDAO();
-		long codUs = usuarioDAO.cadastrarUsuario(usuario);
-		usuario.setId(codUs);
-		Cliente cliente = new Cliente();
-		cliente.setDadosPessoais(pessoa);
-		cliente.setDadosUsuario(usuario);
-		cliente.setAvaliacao(10);
-		ClienteDAO clienteDAO = new ClienteDAO();
-		long codCl = clienteDAO.cadastraCliente(cliente);
-		cliente.setId(codCl);
-		cliente = clienteDAO.getClienteById((long) 1);
-		Toast.makeText(MainActivity.this,String.valueOf(cliente.getAvaliacao()),Toast.LENGTH_LONG).show();;
-		 **/
-
-		/*Cliente cliente;
-		Usuario usuario;
-		UsuarioDAO usuarioDAO = new UsuarioDAO();
-		ClienteDAO clienteDAO = new ClienteDAO();
-		cliente = clienteDAO.getClienteById((long) 5);
-		//cliente.setDadosUsuario(usuarioDAO.getUsuario("teste@gmail.com"));
-		Toast.makeText(MainActivity.this, String.valueOf(cliente.getUsuario().getId()),Toast.LENGTH_LONG).show();
-
-		Usuario usuario = new Usuario();
-		usuario.setEmail("teste@gmail.com");
-		usuario.setSenha("12345");
-		UsuarioDAO usuarioDAO = new UsuarioDAO();
-		long codUs = usuarioDAO.cadastrarUsuario(usuario);
-		usuario.setId(codUs);
-
-		Pessoa pessoa = new Pessoa();
-		pessoa.setCpf("012213124");
-		pessoa.setNome("testenome");
-		PessoaDAO pessoaDAO = new PessoaDAO();
-		long codPessoa =pessoaDAO.cadastraPessoa(pessoa);
-
-		Endereco end = new Endereco();
-		end.setUf("teste");
-		end.setCep("123456");
-		end.setNumero(121);
-		end.setComplemento("Rua 2");
-		end.setBairro("teste");
-		end.setLogradouro("teste");
-		end.setCidade("teste");
-		end.setFkPessoa(codPessoa);
-		EnderecoDAO enderecoDAO= new EnderecoDAO();
-		long codCurr =enderecoDAO.cadastraEndereco(end);
-		end.setId(codCurr);
-
-		Medico medico = new Medico();
-		medico.setAvaliacao(5.0);
-		medico.setCrmv("teste");
-		medico.setDadosPessoais(pessoa);
-		medico.setUsuario(usuario);*/
 
 
+		butClinica = findViewById(R.id.buttonClinica);
+		butVet = findViewById(R.id.buttonVeterinrario);
+		butClinica = findViewById(R.id.buttonClinica);
 
-	}
-	public void loginClinica(View view){
+        butClinica.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginClinica = new Intent(getBaseContext(), LoginClinicaActivity.class);
+                startActivity(loginClinica);
+            }
+        });
 
-    }
+        butVet.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginMedico = new Intent(getBaseContext(), LoginClinicaActivity.class);
+                startActivity(loginMedico);
+            }
+        });
 
-	public void loginMedico(View view){
-
-	}
-	public void loginCliente(View view){
 
 	}
 }
