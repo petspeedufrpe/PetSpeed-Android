deaadf0e01594b70697ae4c64fa6015c906d9998
NEWUSER <caiofariaspe@gmail.com>
2019-06-03 16:02:36 -0300

    Integração Crud Animal na Home do cliente

--- app/src/main/java/br/ufrpe/bsi/mpoo/petSpeed/infra/gui/FinalizaCadastroActivity.java
+++ app/src/main/java/br/ufrpe/bsi/mpoo/petSpeed/infra/gui/FinalizaCadastroActivity.java
@@ -1,15 +1,13 @@
 package br.ufrpe.bsi.mpoo.petSpeed.infra.gui;
 
-import android.app.Dialog;
-import android.app.ProgressDialog;
 import android.content.Intent;
+import android.location.Geocoder;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.Window;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import java.io.IOException;
@@ -23,6 +21,7 @@ import br.ufrpe.bsi.mpoo.petSpeed.infra.negocio.ApiGeocoder;
 import br.ufrpe.bsi.mpoo.petSpeed.infra.negocio.ApiRequestService;
 import br.ufrpe.bsi.mpoo.petSpeed.infra.negocio.AppException;
 import br.ufrpe.bsi.mpoo.petSpeed.infra.negocio.ContasDeUsuario;
+import br.ufrpe.bsi.mpoo.petSpeed.infra.negocio.GeocodeRequestCallbackListener;
 import br.ufrpe.bsi.mpoo.petSpeed.infra.negocio.SessaoCadastro;
 import br.ufrpe.bsi.mpoo.petSpeed.medico.dominio.Medico;
 import br.ufrpe.bsi.mpoo.petSpeed.medico.negocio.MedicoServices;
@@ -31,13 +30,11 @@ import br.ufrpe.bsi.mpoo.petSpeed.pessoa.negocio.PessoaServices;
 
 public class FinalizaCadastroActivity extends AppCompatActivity {
 
-    private ApiGeocoder geocoder = new ApiGeocoder(PetSpeedApp.getContext());
+    RegisterTask registerTask = null;
+
+    boolean isRegisterTaskRunning = false;
 
     Button btnCancelar, btnCadastrar;
-
-    boolean taskRunning;
-
-    private TaskEndereco mTaskEndereco = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,27 +42,21 @@ public class FinalizaCadastroActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_finaliza_cadastro);
 
-
         btnCadastrar = (Button) findViewById(R.id.finaliza_cadastro);
         btnCancelar = (Button) findViewById(R.id.cancela_cadastro);
-        final Intent registerEnd = getIntent();
 
         btnCadastrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              if(!taskRunning){
-                  mTaskEndereco = new TaskEndereco();
-                  mTaskEndereco.execute((Void)null);
-                  ApiRequestService req = new ApiRequestService();
-                  req.geocodeReq("jadshasgd", new ApiRequestService.RequestCallback() {
-                      @Override
-                      public void onCallback() {
-                          //todo/
-                      }
-                  });
-              }else{
-                  Toast.makeText(FinalizaCadastroActivity.this,"Processando...",Toast.LENGTH_SHORT).show();
-              }
+                if (isRegisterTaskRunning) {
+                    Toast.makeText(FinalizaCadastroActivity.this, "Processando...", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                if (registerTask == null) {
+                    isRegisterTaskRunning = true;
+                    registerTask = new RegisterTask();
+                    registerTask.execute((Void) null);
+                }
             }
         });
 
@@ -74,15 +65,60 @@ public class FinalizaCadastroActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Toast.makeText(FinalizaCadastroActivity.this, "Cadastro cancelado.", Toast.LENGTH_LONG).show();
                 startActivity(new Intent(FinalizaCadastroActivity.this, LoginActivity.class));
-                finish();
             }
         });
     }
 
-    private void cadastrar() throws IOException {
-        ContasDeUsuario tipo = SessaoCadastro.instance.getTipo();
+    private class RegisterTask extends AsyncTask<Void, Void, Void> {
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            String enderecoF = getEnderecoFormated();
+            ApiRequestService geocodeReq = new ApiRequestService();
+            geocodeReq.geocodeRequest(enderecoF, new GeocodeRequestCallbackListener<Map<Enum, Object>>() {
+                @Override
+                public void onGeocodeCallback(Map latLng) {
+                    if ((Enum) latLng.get(ApiRequestService.GeoCodeCoord.RESULT) == ApiRequestService.GeoCodeCoord.SUCCESS) {
+                        double lat = (double) latLng.get(ApiRequestService.GeoCodeCoord.LAT);
+                        double lng = (double) latLng.get(ApiRequestService.GeoCodeCoord.LNG);
+                        Endereco endereco = SessaoCadastro.instance.getEndereco();
+                        endereco.setLatidude(lat);
+                        endereco.setLongitude(lng);
+                        try {
+                            cadastrar(endereco);
+                            startActivity(new Intent(FinalizaCadastroActivity.this, LoginActivity.class));
+                            finish();
+                            registerTask = null;
+                            isRegisterTaskRunning = false;
+                        } catch (AppException e) {
+                            Toast.makeText(FinalizaCadastroActivity.this, "Ops! parece que algo deu errado.", Toast.LENGTH_LONG).show();
+                            registerTask = null;
+                            isRegisterTaskRunning = false;
+                        }
+                    } else {
+                        Toast.makeText(FinalizaCadastroActivity.this, "Por favor, verifique sua conexão com a internet.", Toast.LENGTH_LONG).show();
+                        registerTask = null;
+                        isRegisterTaskRunning = false;
+                    }
+                }
+            });
+            return null;
+        }
+    }
+
+    private String getEnderecoFormated() {
         Endereco endereco = SessaoCadastro.instance.getEndereco();
-        setLatLong(endereco);
+        StringBuilder parserEndereco = new StringBuilder();
+        parserEndereco.append(endereco.getLogradouro());
+        parserEndereco.append(", ");
+        parserEndereco.append(endereco.getNumero());
+        parserEndereco.append(" ");
+        parserEndereco.append(endereco.getBairro());
+        return parserEndereco.toString();
+    }
+
+    private void cadastrar(Endereco endereco) throws AppException {
+        ContasDeUsuario tipo = SessaoCadastro.instance.getTipo();
         if (tipo == ContasDeUsuario.CLIENTE) {
             Cliente cliente = SessaoCadastro.instance.getCliente();
             cliente.getDadosPessoais().setEndereco(endereco);
@@ -93,7 +129,7 @@ public class FinalizaCadastroActivity extends AppCompatActivity {
             cadastraMedico(medico);
         } else if (tipo == ContasDeUsuario.CLINICA) {
         } else {
-            //Toast.makeText(FinalizaCadastroActivity.this, "Ops! parece que algo deu errado.", Toast.LENGTH_LONG).show();
+            throw new AppException("Erro");
         }
     }
 
@@ -104,64 +140,25 @@ public class FinalizaCadastroActivity extends AppCompatActivity {
         cliente.getDadosPessoais().setId(idPessoa);
         ClienteServices clienteServices = new ClienteServices();
         clienteServices.cadastraCliente(cliente, cliente.getUsuario());
-        //Toast.makeText(FinalizaCadastroActivity.this, "Cadastro realizado.", Toast.LENGTH_LONG).show();
+        Toast.makeText(FinalizaCadastroActivity.this, "Cadastro realizado.", Toast.LENGTH_LONG).show();
     }
 
-    private void cadastraMedico(Medico medico) {
+    private void cadastraMedico(Medico medico) throws AppException {
         PessoaServices pessoaServices = new PessoaServices();
         long idPessoa = pessoaServices.cadastraPessoa(medico.getDadosPessoais(), medico.getDadosPessoais().getEndereco());
         medico.getDadosPessoais().setId(idPessoa);
         MedicoServices medicoServices = new MedicoServices();
-        try {
-            medicoServices.cadastraMedico(medico, medico.getUsuario());
-        } catch (AppException e) {
-           // Toast.makeText(FinalizaCadastroActivity.this, "Ops! parece que algo deu errado.", Toast.LENGTH_LONG).show();
-
-        }
+        medicoServices.cadastraMedico(medico, medico.getUsuario());
         Toast.makeText(FinalizaCadastroActivity.this, "Cadastro realizado.", Toast.LENGTH_LONG).show();
     }
 
-    public void setLatLong(Endereco endereco) throws IOException {
+    /*public void setLatLong(Endereco endereco) throws IOException {
         StringBuilder parserEndereco = new StringBuilder();
         parserEndereco.append(endereco.getLogradouro());
         parserEndereco.append(", ");
         parserEndereco.append(endereco.getNumero());
-        Map<String,Double> latlngCoord = geocoder.getPositions(parserEndereco.toString());
-          endereco.setLatidude(latlngCoord.get(ApiGeocoder.GeocodeLatLng.LAT.getStr()));
-          endereco.setLongitude(latlngCoord.get(ApiGeocoder.GeocodeLatLng.LNG.getStr()));
-    }
-
-    public class TaskEndereco extends AsyncTask<Void,Void,Void>{
-        //ProgressDialog load;
-        @Override
-        protected void onPreExecute() {
-            //load = ProgressDialog.show(FinalizaCadastroActivity.this,"Aguarde","");
-        }
-
-        @Override
-        protected Void doInBackground(Void... voids) {
-            try {
-                taskRunning=true;
-                cadastrar();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            resetTask();
-            startActivity(new Intent(FinalizaCadastroActivity.this,LoginActivity.class));
-        }
-
-        private void resetTask() {
-            mTaskEndereco = null;
-        }
-
-        @Override
-        protected void onCancelled() {
-            resetTask();
-        }
-    }
+        Map<String, Double> latlngCoord = geocoder.getPositions(parserEndereco.toString());
+        endereco.setLatidude(latlngCoord.get(ApiGeocoder.GeocodeLatLng.LAT.getStr()));
+        endereco.setLongitude(latlngCoord.get(ApiGeocoder.GeocodeLatLng.LNG.getStr()));
+    }*/
 }
--- app/src/main/java/br/ufrpe/bsi/mpoo/petSpeed/infra/negocio/ApiRequestService.java
+++ app/src/main/java/br/ufrpe/bsi/mpoo/petSpeed/infra/negocio/ApiRequestService.java
@@ -14,22 +14,21 @@ import com.android.volley.toolbox.JsonObjectRequest;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONStringer;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import br.ufrpe.bsi.mpoo.petSpeed.infra.app.PetSpeedApp;
-import br.ufrpe.bsi.mpoo.petSpeed.pessoa.dominio.Endereco;
 
 public class ApiRequestService {
 
-    public interface RequestCallback{
-        void onCallback();
-    }
 
-    public void geocodeReq(String urlRequest, final RequestCallback callback) {
+    public void geocodeRequest(final String address, final GeocodeRequestCallbackListener listener) {
 
-        final ArrayList<Double>latlong = new ArrayList<>();
+        String url = getGeocodeUrl(address);
+
+        final ArrayList<Double> latlong = new ArrayList<>();
 
         Cache cache = new DiskBasedCache(PetSpeedApp.getContext().getCacheDir(), 1024 * 1024); // 1MB cap
 
@@ -39,31 +38,35 @@ public class ApiRequestService {
 
         mQueue.start();
 
-        String url = urlRequest;
 
-        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+        final JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
                 try {
+                    Map<Enum, Object> latLng = new HashMap<Enum, Object>();
 
                     JSONArray jsonResults = response.getJSONArray("results");
                     JSONObject jsonData = jsonResults.getJSONObject(0);
                     JSONObject jsonGeometry = jsonData.getJSONObject("geometry");
                     JSONObject jsonLocation = jsonGeometry.getJSONObject("location");
+
                     Object jsonFormatterAddress = jsonData.get("formatted_address");
                     String address = jsonFormatterAddress.toString();
-                    double latitude = jsonLocation.getDouble(geoCodeCoord.LAT.getStr());
-                    double longitude = jsonLocation.getDouble(geoCodeCoord.LNG.getStr());
 
-                    Sessao.instance.setValue(geoCodeCoord.ADDRESS.getStr(),address);
-                    Sessao.instance.setValue(geoCodeCoord.LAT.getStr(),latitude);
-                    Sessao.instance.setValue(geoCodeCoord.LNG.getStr(),longitude);
+                    double latitude = jsonLocation.getDouble(ApiRequestService.GeoCodeCoord.LAT.getString());
+                    double longitude = jsonLocation.getDouble(ApiRequestService.GeoCodeCoord.LNG.getString());
 
+                    latLng.put(GeoCodeCoord.RESULT, GeoCodeCoord.SUCCESS);
+                    latLng.put(GeoCodeCoord.LAT, latitude);
+                    latLng.put(GeoCodeCoord.LNG, longitude);
+                    latLng.put(GeoCodeCoord.ADDRESS, address);
 
-                    callback.onCallback();//Fazer uma sessão para inserir num map as infos de lat long
+                    listener.onGeocodeCallback(latLng);
 
                 } catch (JSONException e) {
-                    e.printStackTrace();
+                    Map<Enum, Object> latLng = new HashMap<Enum, Object>();
+                    latLng.put(GeoCodeCoord.RESULT, GeoCodeCoord.FAIL);
+                    listener.onGeocodeCallback(latLng);
                 }
             }
 
@@ -76,19 +79,32 @@ public class ApiRequestService {
         mQueue.add(request);
     }
 
-    public enum geoCodeCoord {
-        LAT("lat"), LNG("lng"),ADDRESS("endereco");
+    private String getGeocodeUrl(String address) {
+        return "https://maps.googleapis.com/maps/api/geocode/json?address=" + address +
+                "&key=" + "AIzaSyBhntuU8NDLx8ZoIIfxnNRXaziGPvtEB6s";
+    }
+
+    private String getApiKey() {
+        return "@strings/google_maps_key";
+    }
+
+    public enum GeoCodeCoord {
+        LAT("lat"), LNG("lng"), RESULT("result"), ADDRESS("endereco"),
+        ZERO_RESULTS("ZERO_RESULTS"), SUCCESS("success"),
+        FAIL("fail");
         private final String descricao;
-        geoCodeCoord(String descricao) {
+
+        GeoCodeCoord(String descricao) {
             this.descricao = descricao;
         }
-        public String getStr() {
+
+        public String getString() {
             return descricao;
         }
+
         @Override
         public String toString() {
             return this.descricao;
         }
     }
-
 }
