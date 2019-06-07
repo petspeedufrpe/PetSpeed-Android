db0a734b45058473072b5a0e19fced918d77d058
Arthur Queiroz Claizoni <arthurclaizoni@gmail.com>
2019-06-07 00:20:23 -0300

    fix

--- app/src/main/java/br/ufrpe/bsi/mpoo/petSpeed/infra/gui/MapsFragment.java
+++ app/src/main/java/br/ufrpe/bsi/mpoo/petSpeed/infra/gui/MapsFragment.java
@@ -151,7 +151,7 @@ public class MapsFragment extends SupportMapFragment implements OnMapReadyCallba
         markerOptions.position(latLng);
         markerOptions.title(title);
         markerOptions.snippet("média: " + avaliacao);
-        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(82.0f));
+        markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
         marker = mMap.addMarker(markerOptions);
         marker.setTag(mapConta);
         marker.showInfoWindow();
